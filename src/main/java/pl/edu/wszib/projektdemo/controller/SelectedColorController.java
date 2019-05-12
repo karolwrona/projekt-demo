fddbbec24e02317e68fd7fba6ea5cf2a3c6bf976
kwronski <karol.mikolaj.wronski@gmail.com>
2019-05-12 15:52:41 +0200

    15:52

--- src/main/java/pl/edu/wszib/projektdemo/controller/SelectedColorController.java
+++ src/main/java/pl/edu/wszib/projektdemo/controller/SelectedColorController.java
@@ -1,9 +1,17 @@
 package pl.edu.wszib.projektdemo.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.thymeleaf.util.StringUtils;
+import pl.edu.wszib.projektdemo.dao.SelectedColorDao;
+import pl.edu.wszib.projektdemo.domain.SelectedColor;
+
+import java.util.Date;
 
 @Controller
 public class SelectedColorController {
@@ -11,12 +19,21 @@ public class SelectedColorController {
     @Value("${app.header.select_color}")
     private String title;
 
-    @GetMapping("/select")
-    public String selectColorPage(Model model){
+    @Autowired
+    SelectedColorDao selectedColorDao;
+
+    @GetMapping({"/select", "/select/{color}"})
+    public String selectColorPage(@PathVariable(required = false) String color, Model model){
+
+        if(!StringUtils.isEmpty(color)){
+        selectedColorDao.save(new SelectedColor(color, new Date()));
+    }
 
         String[][] colors = {
-                {"red","blue"},
-                {"black, yellow"}
+                {"Silver","Blue", "Red", "Gray"},
+                {"Black", "Yellow", "Purple", "Green"},
+                {"Orange", "Lime", "White","Aqua"},
+                {"Fuchsia", "Teal", "Navy", "Olive"}
         };
 
         model.addAttribute("title", title);
