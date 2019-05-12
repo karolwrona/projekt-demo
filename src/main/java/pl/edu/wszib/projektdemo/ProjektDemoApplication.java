498dbc8be5447b94497d98dbfa806d6922400107
Karol Mikołaj Wroński <kwronski@student.wszib.edu.pl>
2019-05-12 11:13:05 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/projektdemo/ProjektDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projektdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/projektdemo/ProjektDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.projektdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjektDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
